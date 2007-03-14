16a21efde4fb918e90240210dcfc7489fe12d3d7
Mikhail Sobolev <mss@mawhrin.net>
2007-03-14 09:13:12 +0000

    updated the copyright year
    git-svn-id: https://vcs.maemo.org/svn/simple-launcher/trunk@78 3ba93dab-e023-0410-b42a-de7732cf370a

--- launcher-item.h
+++ launcher-item.h
@@ -1,6 +1,6 @@
 // This file is a part of Simple Launcher
 //
-// Copyright (C) 2006, Mikhail Sobolev
+// Copyright (C) 2006, 2007, Mikhail Sobolev
 //
 // Simple Launcher is free software; you can redistribute it and/or modify it
 // under the terms of the GNU General Public License version 2 as published by
