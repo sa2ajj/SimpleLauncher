eab0597ac2bff719f0b7b74516008b198f2bdb14
Mikhail Sobolev <mss@mawhrin.net>
2007-04-12 10:45:48 +0000

    added a really simple test case :)
    git-svn-id: https://vcs.maemo.org/svn/simple-launcher/trunk@174 3ba93dab-e023-0410-b42a-de7732cf370a

--- test2.cc
+++ test2.cc
@@ -3,5 +3,13 @@
 #include "gconf-wrapper.h"
 
 int main() {
-	return 0;
+  g_type_init();
+
+  GConfKey sla("/apps/simple-launcher");
+
+  GConfBooleanOption transparency(sla, "transparency", false);
+
+  std::cout << "Value: " << transparency.value() << std::endl;
+
+  return 0;
 }
